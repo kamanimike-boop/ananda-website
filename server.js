6e34f399efa6b1b4fddc79a84cbd6a329c41706d
kamanimike-boop <kamanimike@gmail.com>
2026-09-20 07:39:59 +0300

    Add WhatsApp paid order notifications

--- server.js
+++ server.js
@@ -6,6 +6,35 @@ const path = require("path");
 const app = express();
 
 /* =========================================================
+   CORS
+========================================================= */
+
+app.use((req, res, next) => {
+  const origin = req.headers.origin;
+
+  if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Vary", "Origin");
+  }
+
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET,POST,PUT,PATCH,DELETE,OPTIONS"
+  );
+
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Content-Type, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
+
+/* =========================================================
    BASIC EXPRESS SETUP
 ========================================================= */
 
